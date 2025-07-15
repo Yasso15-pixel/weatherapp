8218cd424fc7c8c5349d0380a2ce8c52d6a31ea2
Yassin Mohamed <yassin@example.com>
2025-07-15 19:21:24 +0300

    fuewugf

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}', // For Next.js app directory
+    './pages/**/*.{js,ts,jsx,tsx}', // If using pages/
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
